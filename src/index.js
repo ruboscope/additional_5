31a63619280fba89a49f0272c9d0be00768ca827
Zhukov Andrey <AS.Zhukow@BelVeb.by>
2018-02-26 09:54:06 +0300

    - additional_5/feature: заложена идея решения задачи с помощью стека

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  // реализация будет с помощью стека. Если элемент открывающий, то мы будем push в массив, в противном случае будем pop из массива
+  var stack = []; 
+
+  for (var i = 0; i < str.length; i++){
+
+  }
 }
